d5bb5c3a68ed9855785032aa804f6a8de77a0564
MEGUMMY1 <cys10902@nate.com>
2024-07-27 01:56:04 +0900

    Refactor: hover 효과 추가

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useSetRecoilState } from 'recoil';
 import { modalState } from '@/states/modalState';
 import ModalContent from '../Modal/ModalContent/ModalContent';
+import { motion } from 'framer-motion';
 
 export default function AboutMe() {
   const setModal = useSetRecoilState(modalState);
@@ -27,7 +28,14 @@ export default function AboutMe() {
           className={styles.image}
         />
       </div>
-      <div className={styles.bubble_container}>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false }}
+        transition={{ ease: 'easeInOut', duration: 2, y: { duration: 1 } }}
+        className={styles.bubble_container}
+        id="activity"
+      >
         <div className={styles.speech_bubble}>
           안녕하세요. Front-end 주니어 개발자 조혜진입니다.
         </div>
@@ -44,7 +52,7 @@ export default function AboutMe() {
           누구나 편리하게 사용할 수 있도록 사용자의 입장을 생각하고, 서비스를
           만드는 개발자가 되고 싶습니다.
         </div>
-      </div>
+      </motion.div>
       <button className={styles.review_button} onClick={openModal} tabIndex={0}>
         팀원 리뷰 보러가기
       </button>
