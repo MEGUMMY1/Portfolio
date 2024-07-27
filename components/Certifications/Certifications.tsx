67211f15b21b93c3052f2784881a8df20db67eb6
MEGUMMY1 <cys10902@nate.com>
2024-07-27 18:20:00 +0900

    Design: Certifications page 모바일 반응형 추가

--- components/Certifications/Certifications.tsx
+++ components/Certifications/Certifications.tsx
@@ -7,21 +7,27 @@ export default function Certifications() {
     <div className={styles.container}>
       <h1 className={styles.title}>Certifications</h1>
       <div className={styles.button_container}>
-        <Certification
-          title="정보처리기사"
-          date="2023.11"
-          host="과학기술정보통신부"
-        />
-        <Certification
-          title="컴퓨터활용능력 1급"
-          date="2022.03"
-          host="대한상공회의소"
-        />
-        <Certification
-          title="GTQ 포토샵 1급"
-          date="2021.03"
-          host="한국생산성본부"
-        />
+        <div className={styles.button1}>
+          <Certification
+            title="정보처리기사"
+            date="2023.11"
+            host="과학기술정보통신부"
+          />
+        </div>
+        <div className={styles.button2}>
+          <Certification
+            title="컴퓨터활용능력 1급"
+            date="2022.03"
+            host="대한상공회의소"
+          />
+        </div>
+        <div className={styles.button3}>
+          <Certification
+            title="GTQ 포토샵 1급"
+            date="2021.03"
+            host="한국생산성본부"
+          />
+        </div>
       </div>
     </div>
   );
