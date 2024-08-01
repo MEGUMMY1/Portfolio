9d1a893fe894d3ccfd72f4ea2317bfdb7f319ac5
MEGUMMY1 <cys10902@nate.com>
2024-08-01 18:17:56 +0900

    Docs: meta tag 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,6 +32,23 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>HYEJEAN CHO</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="조혜진의 포트폴리오" />
+        <meta
+          property="og:description"
+          content="프론트엔드 개발자 조혜진의 포트폴리오입니다."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/57613101?v=4"
+        />
+        <meta property="og:url" content="https://chohyejean.netlify.app/" />
+        <meta property="og:type" content="website" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="프론트엔드 개발자 조혜진의 포트폴리오입니다."
+        />
       </Head>
       <RecoilRoot>
         {isLoading ? (
