709b69abe4d3dab62b5a518bf7313dd9d8abdd9a
MEGUMMY1 <cys10902@nate.com>
2024-09-05 12:59:52 +0900

    Docs: 팀원 리뷰 업데이트

--- components/Modal/ModalContent/ModalContent.tsx
+++ components/Modal/ModalContent/ModalContent.tsx
@@ -1,53 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './ModalContent.module.scss';
-
-const tabs = [
-  {
-    label: '강　점',
-    content: (
-      <ul className={styles.line}>
-        <li className={styles.item}>
-          팀장으로서 팀을 잘 이끌어주신 점이 인상깊었습니다.
-        </li>
-        <li className={styles.item}>
-          팀 분위기 메이커! 어색함 없이 팀원들과 소통하고 편한 분위기를
-          만들어주셔서 팀 활동이 더 즐거웠습니다.
-        </li>
-        <li className={styles.item}>
-          사소한 질문에도 세세하게 답변해줌, 남들이 안 하려는 일을 자진해서
-          지원함.
-        </li>
-        <li className={styles.item}>
-          팀원이 어려워 하는 파트를 대신 맡아서 해주었습니다.
-        </li>
-        <li className={styles.item}>
-          분배가 안된 파트를 알아서 다 해주셨습니다.
-        </li>
-        <li className={styles.item}>
-          밤낮 구분없이 작업을 해 마감일 안에 프로젝트를 잘 마무리 할 수
-          있었습니다.
-        </li>
-      </ul>
-    ),
-  },
-  {
-    label: '보완해야 할 점',
-    content: (
-      <ul className={styles.line}>
-        <li className={styles.item}>
-          팀 프로젝트 및 개인 학습 목표 설정 시 좀 더 명확하게 하여 팀원들과
-          공유하면 더 좋을 것 같습니다.
-        </li>
-        <li className={styles.item}>
-          불만이나 피드백을 조금 명확하고 뚜렷하게 말씀해주시면 좋을 것 같다.
-        </li>
-      </ul>
-    ),
-  },
-];
+import contentData from './content.json';
+import { Tab } from './ModalContent.types';
 
 export default function ModalContent() {
   const [activeTab, setActiveTab] = useState(0);
+  const [tabs, setTabs] = useState<Tab[]>([]);
+
+  useEffect(() => {
+    setTabs(contentData.tabs);
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -64,7 +26,17 @@ export default function ModalContent() {
           </button>
         ))}
       </div>
-      <div className={styles.tab_content}>{tabs[activeTab].content}</div>
+      <div className={styles.tab_content}>
+        {tabs[activeTab] && (
+          <ul className={styles.line}>
+            {tabs[activeTab].content.map((item, idx) => (
+              <li key={idx} className={styles.item}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
