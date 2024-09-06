8dea3d6430ff094a86d31c3d4f646687f0ef4877
MEGUMMY1 <cys10902@nate.com>
2024-09-06 18:22:01 +0900

    Docs: 팀원 리뷰 수정

--- components/Modal/ModalContent/ModalContent.tsx
+++ components/Modal/ModalContent/ModalContent.tsx
@@ -1,42 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './ModalContent.module.scss';
 import contentData from './content.json';
-import { Tab } from './ModalContent.types';
 
 export default function ModalContent() {
-  const [activeTab, setActiveTab] = useState(0);
-  const [tabs, setTabs] = useState<Tab[]>([]);
-
-  useEffect(() => {
-    setTabs(contentData.tabs);
-  }, []);
-
   return (
     <div className={styles.container}>
-      <div className={styles.tabs}>
-        {tabs.map((tab, index) => (
-          <button
-            key={index}
-            className={`${styles.tab} ${
-              activeTab === index ? styles.active : ''
-            }`}
-            onClick={() => setActiveTab(index)}
-          >
-            {tab.label}
-          </button>
+      <ul className={styles.content}>
+        {contentData.map((item, idx) => (
+          <li key={idx} className={styles.item}>
+            {item}
+          </li>
         ))}
-      </div>
-      <div className={styles.tab_content}>
-        {tabs[activeTab] && (
-          <ul className={styles.line}>
-            {tabs[activeTab].content.map((item, idx) => (
-              <li key={idx} className={styles.item}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      </ul>
     </div>
   );
 }
